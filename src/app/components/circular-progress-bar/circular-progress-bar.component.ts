87df5735af878fa0b7b8b77cf1788fa5afb79916
enessusan00 <menric.drago@gmail.com>
2023-11-02 23:33:17 +0300

    Add circular progress bar component with increase and decrease functionality

--- src/app/components/circular-progress-bar/circular-progress-bar.component.ts
+++ src/app/components/circular-progress-bar/circular-progress-bar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'circular-progress-bar',
   templateUrl: './circular-progress-bar.component.html',
-  styleUrls: ['./circular-progress-bar.component.css']
+  styleUrls: ['./circular-progress-bar.component.css'],
 })
-export class CircularProgressBarComponent {
+export class CircularProgressBarComponent implements OnInit {
+  // radius variable to store the radius of the progress bar
+  circumference: number = 30 * 3.2 * Math.PI;
+  // percent variable to store the percentage of the progress bar
+  percent: number = 0;
+  constructor() { }
 
+  ngOnInit(): void {
+  }
+  // increase function to increase the progress bar
+  increase() {
+    this.percent += 10;
+    if (this.percent > 100) {
+      this.percent = 100;
+    }
+  }
+  // decrease function to decrease the progress bar
+  decrease() {
+    this.percent -= 10;
+    if (this.percent < 0) {
+      this.percent = 0;
+    }
+  }
 }
